dc2bbae1427a2e289e34e2d7ace4a60885b00f17
Andrew Gao <andrew@compound.co>
2021-01-07 14:47:54 -0800

    Working model of the owe/owed screen
    Completed a working model of the owe/owed screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './HomePage/Home'; 
+import OweOwed from './src/screens/OweOwed.js'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-//every page we need 
+//every page we need we add a stack.screen with the component name
 export default function App({navigation}) {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="OweOwed">
       <Stack.Screen
           options={{ title: "" }}
           name="Home"
           component={Home}
+        />
+      <Stack.Screen
+          options={{ title: "" }}
+          name="OweOwed"
+          component={OweOwed}
         />
       </Stack.Navigator>
     </NavigationContainer>
