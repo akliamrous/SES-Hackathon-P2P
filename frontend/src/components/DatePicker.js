04fc9bb4a0480ceed9e72219c7790cf5a1b2c8a4
gwthompson23 <gwthompson23@gmail.com>
2021-01-07 17:52:40 -0500

    Transactions v1

---
+++ frontend/src/components/DatePicker.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Button } from 'react-native-paper';
+
+const DatePicker = () => {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const [selectedDate, setDate] = useState({
+        month: "",
+        day: "",
+        year: ""
+    });
+
+    const [pickerMode, setPickerMode] = useState(null);
+  
+    const showDatePicker = () => {
+        setPickerMode("date");
+    };
+  
+    const hidePicker = () => {
+        setPickerMode(null);
+    };
+  
+    const handleConfirm = (date) => {
+        let monthIndex = date.substring(5,7);
+        setDate({
+            month: months[monthIndex],
+            day: date.substring(9, 11),
+            year: date.substring(0,4)
+        })
+        console.log(selectedDate);
+        hidePicker();
+    };
+  
+    return (
+      <View style={style.root}>
+        <Text style={style.label}>Repay by: </Text>
+        <Button
+            mode="contained" 
+            onPress={showDatePicker}
+        >
+            Choose Date
+        </Button>
+        <DateTimePickerModal
+          isVisible={pickerMode !== null}
+          mode={pickerMode}
+          onConfirm={handleConfirm}
+          onCancel={hidePicker}
+        />
+        <Text>Selected date: {selectedDate.month} {selectedDate.day} {selectedDate.year}</Text>
+      </View>
+    );
+  };
+  
+  const style = StyleSheet.create({
+    root: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    label: {
+        fontWeight: "bold"
+    }
+  });
+
+  export default DatePicker;
---
+++ frontend/src/screens/Transactions.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Searchbar, TextInput, Button } from 'react-native-paper';
+import DatePicker from "../components/DatePicker";
+
+export default function Transactions() {
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const [textAmount, setAmount] = React.useState('');
+
+    const onChangeSearch = query => setSearchQuery(query);
+
+    return (
+        <PaperProvider>
+                <Searchbar
+                    style={styles.searchbar}
+                    placeholder="Enter Name"
+                    onChangeText={onChangeSearch}
+                    value={searchQuery}
+                />
+                <View style={styles.inlineView}>
+                    <TextInput
+                        style={styles.amount}
+                        label="$ Amount"
+                        value={textAmount}
+                        onChangeText={textAmount => setAmount(textAmount)}
+                        keyboardType={'numeric'}
+                    />
+                    <DatePicker />
+                </View>
+                <View style={styles.inlineView}>
+                    <Button 
+                        style={styles.button}
+                        icon="send" 
+                        mode="contained" 
+                        onPress={() => console.log('Pressed')}
+                    >
+                        Lend
+                    </Button>
+                    <Button 
+                        style={styles.button}
+                        icon="request" 
+                        mode="contained" 
+                        onPress={() => console.log('Pressed')}
+                    >
+                        Request
+                    </Button>
+                </View>
+        </PaperProvider>
+      );
+}
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F7EE',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    searchbar: {
+        marginTop: 100,
+        marginBottom: 20,
+        marginHorizontal: 10,
+        width: 320
+    },
+    amount: {
+        marginVertical: 20,
+        marginRight: 50,
+        width: 100
+    },
+    button: {
+        marginVertical: 20,
+        marginHorizontal: 10,
+        display: 'flex',
+        width: 150
+    },
+    inlineView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
