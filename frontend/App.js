af889aea49bc750841ca7519083a2de1c80cae15
Mathew Chanda <mvchanda@ncsu.edu>
2021-01-07 16:05:33 -0500

    finished home

--- frontend/App.js
+++ frontend/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './HomePage/Home'; 
 export default function App() {
+  
   return (
-    <Home/>
+         <Home/>
+    
   );
 }
 
