7e7777bd13e25af0191c09816c074d975795a489
Alice Yeh <aliceyeh12@gmail.com>
2021-01-07 22:30:42 -0800

    Styling landing and auth pages

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -7,9 +7,18 @@ export const globalStyles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
+    inputContainer: {
+        flex: 1,
+        justifyContent: "center",
+        padding: 75,
+        paddingBottom: 200,
+        backgroundColor: "white",
+    },
     inputDescText: {
+        color: "#48B77D",
         fontWeight: "bold",
-        fontSize: 22,
+        fontSize: 18,
+        textTransform: "uppercase",
     },
     errorMsgText: {
         fontSize: 12,
